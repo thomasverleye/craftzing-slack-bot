9818246324bdfb0c42a1f9acb5eae3eba44f66a0
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2022-09-18 16:52:40 +0200

    :wrench: Make bootstrap testable

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,14 +1,18 @@
 import { config as dotenv } from 'dotenv';
 import { register as registerTsConfigPath } from 'tsconfig-paths';
 
-dotenv();
+export const bootstrap = () => {
+  dotenv();
 
-registerTsConfigPath({
-  baseUrl: './',
-  paths: {
-    '*': [
-      './src/*', // dev
-      './*', // dist
-    ],
-  },
-});
+  registerTsConfigPath({
+    baseUrl: './',
+    paths: {
+      '*': [
+        './src/*', // dev
+        './*', // dist
+      ],
+    },
+  });
+};
+
+bootstrap();
