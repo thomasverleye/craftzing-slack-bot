e1dc106ee9eaa730459f157ac0923a0a8f1d4e24
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2022-09-18 16:40:31 +0200

    :white_check_mark: when path provided, and the api upload is succesful, the function should return a url

--- src/services/imgur.spec.ts
+++ src/services/imgur.spec.ts
@@ -1,8 +1,20 @@
 import axios from 'axios';
+import fs from 'fs';
+import { Readable } from 'stream';
 
 import Imgur from './imgur';
 
+jest.mock('fs');
 jest.mock('axios');
+
+const mockReadStream = jest.fn().mockImplementation(() => {
+  const readable = new Readable();
+  readable.push('hello');
+  readable.push('world');
+  readable.push(null);
+
+  return readable;
+});
 
 describe('Imgur.upload', () => {
   test('when empty path provided, the function should return false', async () => {
@@ -15,5 +27,30 @@ describe('Imgur.upload', () => {
     // then
     expect(axios.post).toHaveBeenCalledTimes(0);
     expect(response).toBe(false);
+  });
+
+  test('when path provided, and the api upload is succesful, the function should return a url', async () => {
+    // given
+    const path = 'image.jpg';
+    jest.spyOn(fs, 'createReadStream').mockReturnValueOnce(mockReadStream());
+    jest.spyOn(axios, 'post').mockReturnValueOnce(
+      new Promise((resolve) =>
+        resolve({
+          status: 200,
+          data: {
+            success: true,
+            data: { link: 'https://i.imgur.com/123456.jpg' },
+          },
+        }),
+      ),
+    );
+
+    // when
+    const response = await Imgur.upload(path);
+
+    // then
+    expect(fs.createReadStream).toHaveBeenCalledTimes(1);
+    expect(axios.post).toHaveBeenCalledTimes(1);
+    expect(response).toContain('https://i.imgur.com');
   });
 });
