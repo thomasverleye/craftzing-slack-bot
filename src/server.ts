64f51ca99e4495b20deea265e57f2e52284754d6
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2022-09-19 23:50:12 +0200

    :alembic: Try out reading & responding to message

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,20 @@
-import Imgur from 'services/imgur';
-
-console.log('Hello World');
+import { App as Slack } from '@slack/bolt';
 
 (async () => {
-  console.log('Uploading image...');
-  const link = await Imgur.upload('./goed-gedaan.jpg');
-  console.log({ link });
+  const slack = new Slack({
+    signingSecret: process.env.SLACK_SIGNING_SECRET,
+    token: process.env.SLACK_BOT_TOKEN,
+    socketMode: true,
+    appToken: process.env.SLACK_APP_TOKEN,
+  });
+
+  slack.message('hello', async ({ message, say }) => {
+    console.log({ message });
+
+    await say('hello');
+  });
+
+  await slack.start();
+
+  console.log('⚡️ Slack bot started!');
 })();
