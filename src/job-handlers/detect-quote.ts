c2257e30234205ce038f07f0324d84273cc8a4a8
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2022-10-12 00:22:16 +0200

    :alembic: Quote detection WIP

--- src/job-handlers/detect-quote.ts
+++ src/job-handlers/detect-quote.ts
@@ -1,3 +1,23 @@
+import TextRecognition from 'services/text-recognition';
+
+let lastDetectedQuote: string | null = null;
+
 export const handleDetectQuoteJob = async () => {
-  console.log('hello world');
+  const quote = await TextRecognition.detect('./webcam.jpg');
+  if (!quote) {
+    return;
+  }
+
+  if (quote === lastDetectedQuote) {
+    return;
+  }
+
+  if (lastDetectedQuote) {
+    console.log('New quote detected!', { quote });
+    return;
+  }
+
+  console.log('Quote detected!', { quote });
+
+  lastDetectedQuote = quote;
 };
