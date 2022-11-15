950d6684cf80df94db6475cfacd4396d437fee18
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2022-11-15 22:59:37 +0100

    :rotating_light: Linting™

--- src/message-handlers/index.ts
+++ src/message-handlers/index.ts
@@ -1,7 +1,6 @@
 export * from './avocado';
 export * from './beer-time';
 export * from './croque';
-export * from './define';
 export * from './hello';
 export * from './joke';
 export * from './quote-of-the-day';
