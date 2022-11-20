c9fa465807eb43f1dccef9c844149401de53bd78
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2022-11-20 11:13:21 +0100

    :art: Clearer to read

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,4 @@
 export const AVOCADO_BIRTHDAY = new Date('2022-09-19T12:00:00.000Z');
+export const CHANNELS = {
+  CZ_OFFICE_GENT: 'C023XSSPZSQ',
+};
--- src/message-handlers/avocado.ts
+++ src/message-handlers/avocado.ts
@@ -1,6 +1,6 @@
 import { GenericMessageEvent, SlackEventMiddlewareArgs } from '@slack/bolt';
 import { WebClient } from '@slack/web-api';
-import { AVOCADO_BIRTHDAY } from 'config';
+import { AVOCADO_BIRTHDAY, CHANNELS } from 'config';
 import { format, formatDistanceToNowStrict } from 'date-fns';
 import Imgur from 'services/imgur';
 
@@ -13,8 +13,13 @@ export const handleAvocadoMessage = async (options: Options) => {
   const message = options.message as GenericMessageEvent;
   const { thread_ts } = message;
 
-  // only allow avocado to be posted in #cz-office-gent channel
-  if (message.channel_type === 'channel' && message.channel !== 'C023XSSPZSQ') {
+  if (
+    // if posted in a channel
+    message.channel_type === 'channel' &&
+    // and it is not #cz-office-gent
+    message.channel !== CHANNELS.CZ_OFFICE_GENT
+  ) {
+    // don't post image but reply with emoji
     await client.reactions.add({
       name: 'x',
       channel: message.channel,
